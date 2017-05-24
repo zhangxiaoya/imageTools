9d77a1652026f6a07dcd53f8c713797e258546f1
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-05-24 16:01:14 +0800

    test opencv config

--- imageTools.toGray/main.cpp
+++ imageTools.toGray/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
-using namespace std;
-
-int main()
+int main(int argc, char** argv)
 {
+	auto img = cv::imread("timg.jpg");
+	if(img.empty())
+	{
+		std::cout << "Open Image file failed" << std::endl;
+	}
+	else
+	{
+		cv::imshow("Test Image",img);
+		std::cout << img.channels()<<std::endl;
+		cv::waitKey(0);
+		cv::destroyAllWindows();
+	}
+	system("Pause");
 	return 0;
 }
