fef88549a93148caeefa0e3b2431d8403fc646fd
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-06-05 17:12:26 +0800

    fix some bugs

--- imageTools.combine/main.cpp
+++ imageTools.combine/main.cpp
@@ -4,52 +4,77 @@
 #include <highgui/highgui.hpp>
 #include <iomanip>
 
-DEFINE_string(imagelist, "", "The image list folder");
+DEFINE_string(imageList, "", "The image list folder");
 DEFINE_string(saveFileName, "", "The video file name to save");
+DEFINE_int32(ftps, 25, "The video ftps to save");
+DEFINE_string(nameTemplate, "Frame_%04d", "The video ftps to save");
+DEFINE_string(format, ".png", "The images' format.");
+DEFINE_string(compressFourCC, "yes", "The images' format.");
 
-int main(int argc,char ** argv)
+const auto buffer_count = 300;
+
+int main(int argc, char** argv)
 {
 	std::string usageStr = "";
 	usageStr += "Do combine image list to one video file.\n";
 	usageStr += "Usage:\n";
-	usageStr += "    imageTools.combine --imagelist=imageList folder --saveFileName=videoName.\n";
+	usageStr += "    imageTools.combine --imagelist=imageList folder --saveFileName=videoName --ftps=25 --nameTemplate=Frame_%04d --format=.png --compressFourCC=yes.\n";
 
 	google::SetUsageMessage(usageStr);
 	google::ParseCommandLineFlags(&argc, &argv, true);
 
-	if (FLAGS_imagelist.empty())
+	if (FLAGS_imageList.empty())
 	{
 		std::cout << "Must input image file" << std::endl;
 	}
-	else if(FLAGS_saveFileName.empty())
+	else if (FLAGS_saveFileName.empty())
 	{
 		std::cout << "Must in put save video file name" << std::endl;
 	}
+	else if(FLAGS_compressFourCC.empty())
+	{
+		std::cout << "Compress with MJPG"<<std::endl;
+	}
 	else
 	{
-		char fileName[100];
-		auto imageListFormat = FLAGS_imagelist + "\\resFrame_%04d.png";
-		sprintf_s(fileName, 100, imageListFormat.c_str(),0);
+		auto ftps = FLAGS_ftps;
+
+		char fileName[buffer_count];
+		auto imageListFormat = FLAGS_imageList + "\\\\" + FLAGS_nameTemplate + FLAGS_format;
+		sprintf_s(fileName, buffer_count, imageListFormat.c_str(), 0);
 
 		auto img = cv::imread(fileName);
+		if (img.empty())
+		{
+			std::cout << fileName<<std::endl;
+			std::cout << "Cannot read image file!" << std::endl;;
+			return -1;
+		}
 		auto frameSize = cv::Size(img.cols, img.rows);
 
 		auto saveVideoName = FLAGS_saveFileName;
-		cv::VideoWriter video_writer(saveVideoName,-1,25,frameSize);
+		std::cout << saveVideoName <<std::endl;
 
-		if(video_writer.isOpened())
+		cv::VideoWriter video_writer;
+		if(FLAGS_compressFourCC.empty())
+			video_writer.open(saveVideoName, CV_FOURCC('M', 'J', 'P', 'G'), ftps, frameSize);
+		else
+			video_writer.open(saveVideoName, -1, ftps, frameSize);
+
+		if (video_writer.isOpened())
 		{
 			auto index = 1;
 			while (true)
 			{
-				sprintf_s(fileName, 100, imageListFormat.c_str(), ++index);
+				sprintf_s(fileName, buffer_count, imageListFormat.c_str(), index++);
 				img = cv::imread(fileName);
+
+				if (img.empty())
+					break;
+
 				video_writer << img;
 
 				std::cout << "index : " << std::setw(4) << index << std::endl;
-
-				if(img.empty())
-					break;
 			}
 			video_writer.release();
 			std::cout << "Done!" << std::endl;
