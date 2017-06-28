8f0fbcf5c7c9867ff8e5451e7777812aa5878ced
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-06-28 16:28:34 +0800

    enhence save file name

--- imageTools.combine/main.cpp
+++ imageTools.combine/main.cpp
@@ -7,19 +7,41 @@
 
 DEFINE_string(imageList, "", "The image list folder");
 DEFINE_string(saveFileName, "", "The video file name to save");
-DEFINE_int32(ftps, 25, "The video ftps to save");
-DEFINE_string(nameTemplate, "Frame_%04d", "The video ftps to save");
+DEFINE_int32(ftps, 10, "The video ftps to save");
+DEFINE_string(nameTemplate, "Frame_%08d", "The video ftps to save");
 DEFINE_string(format, ".png", "The images' format.");
-DEFINE_string(compressFourCC, "yes", "The images' format.");
+DEFINE_string(compressFourCC, "no", "The images' format.");
 
 const auto buffer_count = 300;
+
+void ConstructSaveFileName(const google::int32 ftps, std::string& saveVideoName)
+{
+	saveVideoName = "video_ftps_";
+
+	std::stringstream stream;
+	stream << ftps;
+	saveVideoName += stream.str();
+	saveVideoName += "_";
+
+	time_t td;
+	time(&td);
+	auto localtd = localtime(&td);
+
+	const auto size_in_bytes = 256;
+	char date_time[size_in_bytes];
+	strftime(date_time, size_in_bytes, "%Y%m%d_%H%M%S", localtd);
+
+	saveVideoName += date_time;
+
+	saveVideoName += ".avi";
+}
 
 int main(int argc, char** argv)
 {
 	std::string usageStr = "";
 	usageStr += "Do combine image list to one video file.\n";
 	usageStr += "Usage:\n";
-	usageStr += "    imageTools.combine --imagelist=imageList folder --saveFileName=videoName --ftps=25 --nameTemplate=Frame_%04d --format=.png --compressFourCC=yes.\n";
+	usageStr += "    imageTools.combine --imagelist=imageList folder --saveFileName=videoName --ftps=10 --nameTemplate=Frame_%08d --format=.png --compressFourCC=yes/no.\n";
 
 	google::SetUsageMessage(usageStr);
 	google::ParseCommandLineFlags(&argc, &argv, true);
@@ -28,13 +50,9 @@ int main(int argc, char** argv)
 	{
 		std::cout << "Must input image file" << std::endl;
 	}
-	else if (FLAGS_saveFileName.empty())
+	else if (FLAGS_compressFourCC.empty())
 	{
-		std::cout << "Must in put save video file name" << std::endl;
-	}
-	else if(FLAGS_compressFourCC.empty())
-	{
-		std::cout << "Compress with MJPG"<<std::endl;
+		std::cout << "Compress with MJPG" << std::endl;
 	}
 	else
 	{
@@ -47,17 +65,26 @@ int main(int argc, char** argv)
 		auto img = cv::imread(fileName);
 		if (img.empty())
 		{
-			std::cout << fileName<<std::endl;
+			std::cout << fileName << std::endl;
 			std::cout << "Cannot read image file!" << std::endl;;
 			return -1;
 		}
 		auto frameSize = cv::Size(img.cols, img.rows);
 
-		auto saveVideoName = FLAGS_saveFileName;
-		std::cout << saveVideoName <<std::endl;
+		std::string saveVideoName;
+		if (FLAGS_saveFileName.empty())
+		{
+			ConstructSaveFileName(ftps, saveVideoName);
+		}
+		else
+		{
+			saveVideoName = FLAGS_saveFileName;
+		}
+
+		std::cout << saveVideoName << std::endl;
 
 		cv::VideoWriter video_writer;
-		if(FLAGS_compressFourCC.empty())
+		if (FLAGS_compressFourCC.empty())
 			video_writer.open(saveVideoName, CV_FOURCC('M', 'J', 'P', 'G'), ftps, frameSize);
 		else
 			video_writer.open(saveVideoName, -1, ftps, frameSize);
