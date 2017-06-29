12b19ff912c96e6de209897bc8c4d590a0274b86
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-06-29 11:00:12 +0800

    extract google flag definitions to header file

--- imageTools.combineMore/main.cpp
+++ imageTools.combineMore/main.cpp
@@ -2,19 +2,9 @@
 #include <iostream>
 #include <highgui/highgui.hpp>
 #include <iomanip>
-
-#include <gflags/gflags.h>
 #include <imgproc/imgproc.hpp>
 
-DEFINE_string(imageList1, "E:\\WorkLogs\\Data\\Ir\\207\\Raw\\2_500-1500_150ms", "The first image list folder");
-DEFINE_string(imageList2, "E:\\WorkLogs\\Data\\Ir\\207\\Raw\\result\\2_500-1500_150ms", "The second image list folder");
-DEFINE_string(saveFileName, "", "The video file name to save");
-DEFINE_int32(ftps, 10, "The video ftps to save");
-DEFINE_string(nameTemplate1, "Frame_%08d", "The first image list name template");
-DEFINE_string(nameTemplate2, "Frame_%08d", "The second image list name template");
-DEFINE_string(format1, ".png", "The first images' format.");
-DEFINE_string(format2, ".png", "The second images' format.");
-DEFINE_string(compressFourCC, "", "The images' format.");
+#include "googleFlag.h"
 
 const auto buffer_count = 300;
 
