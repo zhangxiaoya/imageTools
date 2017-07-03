c8debaf0059ef1eb2681fd65d9ebf3780f0483e1
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-07-03 09:35:16 +0800

    read image

--- imageTools.strengthenIntensity/main.cpp
+++ imageTools.strengthenIntensity/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <opencv2/core/core.hpp>
+#include <highgui/highgui.hpp>
 
 int main(int argc, char* argv[])
 {
 	std::cout << "This is Strengthen intensity"<<std::endl;
+
+	auto img = cv::imread("E:\\WorkLogs\\Data\\Ir\\207\\Raw\\1_0-600m_150ms\\Frame_00000000.bmp");
+
+	if(img.empty() == true)
+	{
+		std::cout << "Open Image File Failed!" << std::endl;
+		system("Pause");
+		return -1;
+	}
+
+	imshow("Before Strengthen Intensity", img);
+
+	cv::waitKey(0);
+
+	cv::destroyAllWindows();
+
 	system("Pause");
 	return 0;
 }
