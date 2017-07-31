6d40699e6a4f20bb7c45fbb58606e01314a8efe1
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-07-31 18:20:42 +0800

    refactor and add like-equalized generator

--- imageTools.longBinToFrames/main.cpp
+++ imageTools.longBinToFrames/main.cpp
@@ -1,6 +1,5 @@
 #include <core/core.hpp>
 #include <highgui/highgui.hpp>
-
 #include <iostream>
 #include <fstream>
 #include <iomanip>
@@ -68,43 +67,99 @@ int FindMaxDiff(cv::Mat& img, uint16_t& min_val, uint16_t& max_val)
 	return static_cast<int>(maxVal - minVal);
 }
 
-void GenerateSmallFrame(cv::Mat& img, cv::Mat& smallImg, uint16_t min_val)
+void GenerateFrameByLowDifference(cv::Mat& originalImg, cv::Mat& lowDifferenceImg, uint16_t minVal)
 {
-	for (auto r = 0; r < img.rows; ++r)
+	cv::Mat basicImg(ImageHeight, ImageWidth, CV_8UC1);
+	for (auto r = 0; r < originalImg.rows; ++r)
 	{
-		auto ptr = img.ptr<uint16_t>(r);
-		auto smallPtr = smallImg.ptr<uchar>(r);
+		auto ptr = originalImg.ptr<uint16_t>(r);
+		auto smallPtr = basicImg.ptr<uchar>(r);
 
-		for (auto c = 0; c < img.cols; ++c)
+		for (auto c = 0; c < originalImg.cols; ++c)
 		{
 			if (ptr[c] < 255)
 				continue;
-			auto val = ptr[c] - min_val;
-
+			auto val = ptr[c] - minVal;
 			smallPtr[c] = val < 256 ? val : 255;
 			smallPtr[c] = static_cast<uchar>(ceil(smallPtr[c] / 25.0) * 25);
+		}
+	}
+	equalizeHist(basicImg, lowDifferenceImg);
+}
+
+void GenerateLikeEqualizedFrame(const cv::Mat& basicFrame, cv::Mat& likeEqualizeFrame, uint16_t minVal, int diff)
+{
+	int pixelFrequency[256] = {0};
+	double props[256] = {0.0};
+
+	auto scale = (diff + 1) / 256.0;
+
+	for (auto r = 0; r < ImageHeight; ++r)
+	{
+		auto ptr = basicFrame.ptr<uint16_t>(r);
+		for (auto c = 0; c < ImageWidth; ++c)
+		{
+			if (static_cast<uint16_t>(ptr[c]) < minVal)
+			{
+				pixelFrequency[0] ++;
+				continue;
+			}
+			auto pixelVal = static_cast<int>((static_cast<int>(ptr[c]) - static_cast<int>(minVal) + 0.5) / scale);
+
+			if (pixelVal > 255)
+			{
+				std::cout << "Out of Range of Int8" << std::endl;
+				break;
+			}
+			pixelFrequency[pixelVal] ++;
+		}
+	}
+
+	for (auto i = 0; i < 256; ++i)
+	{
+		props[i] = double(pixelFrequency[i]) / double(ImageWidth * ImageHeight);
+
+		if (i != 0)
+			props[i] += props[i - 1];
+	}
+
+	uchar newPixels[256];
+	for (auto i = 0; i < 256; ++i)
+	{
+		newPixels[i] = props[i] * 255;
+	}
+
+	for(auto r = 0;r < ImageHeight;++r)
+	{
+		auto dstPtr = likeEqualizeFrame.ptr<uchar>(r);
+		auto srcPtr = basicFrame.ptr<uint16_t>(r);
+
+		for (auto c = 0; c < ImageWidth; ++c)
+		{
+			auto pixelVal = static_cast<int>((static_cast<int>(srcPtr[c]) - static_cast<int>(minVal) + 0.5) / scale);
+			dstPtr[c] = newPixels[pixelVal];
 		}
 	}
 }
 
 int main(int argc, char* argv[])
 {
-	std::string fullBinFileName = "E:\\WorkLogs\\Projects\\Project4\\Data\\Forth\\test\\ir_file_20170713_300m_jingzhi.bin";
+//	std::string fullBinFileName = "E:\\WorkLogs\\Projects\\Project4\\Data\\Forth\\test\\ir_file_20170713_300m_jingzhi.bin";
 //	std::string fullBinFileName = "E:\\WorkLogs\\Projects\\Project4\\Data\\Forth\\test\\ir_file_20170713_300m_yundong.bin";
 //	std::string fullBinFileName = "E:\\WorkLogs\\Projects\\Project4\\Data\\Forth\\test\\ir_file_20170713_500m_jingzhi.bin";
 //	std::string fullBinFileName = "E:\\WorkLogs\\Projects\\Project4\\Data\\Forth\\test\\ir_file_20170713_500m_yundong.bin";
 //	std::string fullBinFileName = "E:\\WorkLogs\\Projects\\Project4\\Data\\Forth\\test\\ir_file_20170713_700m_jingzhi.bin";
 //	std::string fullBinFileName = "E:\\WorkLogs\\Projects\\Project4\\Data\\Forth\\test\\ir_file_20170713_700m_yundong.bin";
-//	std::string fullBinFileName = "E:\\WorkLogs\\Projects\\Project4\\Data\\Forth\\test\\ir_file_20170713_1000m_jingzhi.bin";
+	std::string fullBinFileName = "E:\\WorkLogs\\Projects\\Project4\\Data\\Forth\\test\\ir_file_20170713_1000m_jingzhi.bin";
 //	std::string fullBinFileName = "E:\\WorkLogs\\Projects\\Project4\\Data\\Forth\\test\\ir_file_20170713_1000m_yundong.bin";
 
-	std::string fullImageFileNameFormat = "E:\\WorkLogs\\Projects\\Project4\\Data\\Forth\\test\\Frames\\ir_file_20170713_300m_jingzhi\\Frame_08%d.png";
+//	std::string fullImageFileNameFormat = "E:\\WorkLogs\\Projects\\Project4\\Data\\Forth\\test\\Frames\\ir_file_20170713_300m_jingzhi\\Frame_08%d.png";
 //	std::string fullImageFileNameFormat = "E:\\WorkLogs\\Projects\\Project4\\Data\\Forth\\test\\Frames\\ir_file_20170713_300m_yundong\\Frame_08%d.png";
 //	std::string fullImageFileNameFormat = "E:\\WorkLogs\\Projects\\Project4\\Data\\Forth\\test\\Frames\\ir_file_20170713_500m_jingzhi\\Frame_08%d.png";
 //	std::string fullImageFileNameFormat = "E:\\WorkLogs\\Projects\\Project4\\Data\\Forth\\test\\Frames\\ir_file_20170713_500m_yundong\\Frame_08%d.png";
 //	std::string fullImageFileNameFormat = "E:\\WorkLogs\\Projects\\Project4\\Data\\Forth\\test\\Frames\\ir_file_20170713_700m_jingzhi\\Frame_08%d.png";
 //	std::string fullImageFileNameFormat = "E:\\WorkLogs\\Projects\\Project4\\Data\\Forth\\test\\Frames\\ir_file_20170713_700m_yundong\\Frame_08%d.png";
-//	std::string fullImageFileNameFormat = "E:\\WorkLogs\\Projects\\Project4\\Data\\Forth\\test\\Frames\\ir_file_20170713_1000m_jingzhi\\Frame_08%d.png";
+	std::string fullImageFileNameFormat = "E:\\WorkLogs\\Projects\\Project4\\Data\\Forth\\test\\Frames\\ir_file_20170713_1000m_jingzhi\\Frame_08%d.png";
 //	std::string fullImageFileNameFormat = "E:\\WorkLogs\\Projects\\Project4\\Data\\Forth\\test\\Frames\\ir_file_20170713_1000m_yundong\\Frame_08%d.png";
 
 	char fullImageFileName[FileNameBufferSize];
@@ -118,7 +173,8 @@ int main(int argc, char* argv[])
 		system("Pause");
 		return -1;
 	}
-	if(GetFileLength(fin) == false)
+
+	if (GetFileLength(fin) == false)
 	{
 		system("Pause");
 		return -1;
@@ -129,10 +185,11 @@ int main(int argc, char* argv[])
 		auto byteIndex = 0;
 		auto rowIndex = 0;
 		auto colIndex = 0;
-		cv::Mat frame(ImageHeight, ImageWidth,CV_16UC1);
-		cv::Mat smallframe(ImageHeight, ImageWidth, CV_8UC1);
 
-		cv::Mat smallSmallframe(ImageHeight, ImageWidth, CV_8UC1);
+		cv::Mat basicFrame(ImageHeight, ImageWidth,CV_16UC1);
+		cv::Mat lowByteFrame(ImageHeight, ImageWidth, CV_8UC1);
+		cv::Mat likeEqualizeFrame(ImageHeight, ImageWidth, CV_8UC1);
+		cv::Mat lowDifferenceFrame;
 
 		while (byteIndex < ImageHeight * ImageWidth * 2)
 		{
@@ -141,13 +198,12 @@ int main(int argc, char* argv[])
 
 			uint16_t perPixel;
 			ConstitudePixel(highPart, lowPart, perPixel);
+			basicFrame.at<int16_t>(rowIndex, colIndex) = perPixel;
 
-			frame.at<int16_t>(rowIndex, colIndex) = perPixel;
-			
-			if (highPart < 0x13)
-				smallSmallframe.at<uchar>(rowIndex, colIndex) = 0;
+			if (highPart < 0x13) // for invalid pixels
+				lowByteFrame.at<uchar>(rowIndex, colIndex) = 0;
 			else
-				smallSmallframe.at<uchar>(rowIndex, colIndex) = lowPart;
+				lowByteFrame.at<uchar>(rowIndex, colIndex) = lowPart;
 
 			ChangeRows(rowIndex, colIndex);
 			byteIndex += 2;
@@ -156,25 +212,26 @@ int main(int argc, char* argv[])
 		uint16_t maxVal = 0;
 		uint16_t minVal = 0;
 
-		auto diff = FindMaxDiff(frame, minVal, maxVal);
-		std::cout << "Max Diff is " << diff << std::endl;
+		auto diff = FindMaxDiff(basicFrame, minVal, maxVal);
 
-		GenerateSmallFrame(frame, smallframe, minVal);
+		GenerateLikeEqualizedFrame(basicFrame, likeEqualizeFrame, minVal, diff);
 
-		sprintf_s(fullImageFileName, FileNameBufferSize, fullImageFileNameFormat.c_str(), frameIndex);
-		imwrite(fullImageFileName, frame);
+		GenerateFrameByLowDifference(basicFrame, lowDifferenceFrame, minVal);
 
-		std::cout << "Current Index = " << std::setw(4)<< frameIndex <<std::endl;
-		imshow("Curent Image", frame);
+		//		sprintf_s(fullImageFileName, FileNameBufferSize, fullImageFileNameFormat.c_str(), frameIndex);
+		//		imwrite(fullImageFileName, basicFrame);
 
-		cv::Mat lastImg;
-		cv::equalizeHist(smallframe, lastImg);
+		std::cout << "Current Index = " << std::setw(4) << frameIndex << std::endl;
 
-		imshow("Small Frame", lastImg);
+		imshow("Basic Frame", basicFrame);
 
-		imshow("Small Small Frame", smallSmallframe);
+		imshow("Low Difference Threshold Frame", lowDifferenceFrame);
 
-		cvWaitKey(1000);
+		imshow("Low Part Original Pixel Frame", lowByteFrame);
+
+		imshow("Like Equalized Frame", likeEqualizeFrame);
+
+		cvWaitKey(300);
 		frameIndex++;
 	}
 
